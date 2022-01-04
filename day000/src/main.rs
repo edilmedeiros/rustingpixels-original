0975b3052b9470b50e1fbe7a5ea2036c1fed0cd3
Edil Medeiros <j.edil@ene.unb.br>
2022-01-04 18:51:48 -0300

    Adjust image size to instagram

--- day000/src/main.rs
+++ day000/src/main.rs
@@ -1,8 +1,8 @@
 use image::Rgb; 
 
 fn main() {
-    let width: u32 = 500;
-    let height: u32 = 500;
+    let width: u32 = 1080;
+    let height: u32 = 1080;
     let bg = Rgb([0x45, 0x7b, 0x9d]);
     
     let image = image::RgbImage::from_pixel(width, height, bg);
