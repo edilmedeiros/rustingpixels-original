9da832463eb318d37388c15c6865e661b2017e48
Edil Medeiros <j.edil@ene.unb.br>
2022-01-04 18:49:37 -0300

    Day 0 - blue

--- day000/src/main.rs
+++ day000/src/main.rs
@@ -1,3 +1,10 @@
+use image::Rgb; 
+
 fn main() {
-    println!("Hello, world!");
+    let width: u32 = 500;
+    let height: u32 = 500;
+    let bg = Rgb([0x45, 0x7b, 0x9d]);
+    
+    let image = image::RgbImage::from_pixel(width, height, bg);
+    image.save("day000.png").unwrap();
 }
