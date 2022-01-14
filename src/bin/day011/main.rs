1f62a506c0f5d34ba38d53f3f9baf2b9988a6565
Edil Medeiros <j.edil@ene.unb.br>
2022-01-13 22:03:42 -0300

    One more interesting set of parameters

--- src/bin/day011/main.rs
+++ src/bin/day011/main.rs
@@ -13,6 +13,7 @@ fn main() {
 
     let mut image1 = image::RgbaImage::from_pixel(width, height, black);
     let mut image2 = image::RgbaImage::from_pixel(width, height, black);
+    let mut image3 = image::RgbaImage::from_pixel(width, height, black);
 
     let matrix = TransformMatrix {
         xx: (width - 1) as f64 / 4.0,
@@ -48,6 +49,19 @@ fn main() {
     }
     image2.save("images/day011-1.png").unwrap();
 
+    let mut p = Point { x: 0.0, y: 0.0 };
+    let a = 1.4;
+    let b = -2.3;
+    let c = 2.4;
+    let d = -2.1;
+    for _i in 0..12_000_000 {
+        let (x, y) = p.point_to_canvas_coordinate(&matrix);
+        image2.get_pixel_mut(x, y).blend(&gray);
+        p = de_jong_ifs(a, b, c, d, p);
+    }
+    image3.save("images/day011-2.png").unwrap();
+
+    
     {
         // Good candidates: 5, 6, 8, 15, 17, 19, 20, 52, 56, 58, 62, 66, 69, 73, 75, 77, 83, 86, 92, 95, 
         // 115, 116, 124, 126, 130, 139, 140, 145, 146, 150, 152, 153, 169, 178, 189, 191, 192, 194, 200,
@@ -72,7 +86,7 @@ fn main() {
             for i in v {
             //for i in 0..1000 {
                 spawner.spawn(move |_| {
-                    let mut image3 = image::RgbaImage::from_pixel(width, height, black);
+                    let mut image = image::RgbaImage::from_pixel(width, height, black);
                     let matrix2 = TransformMatrix {
                         xx: (width - 1) as f64 / 4.0,
                         yx: 0.0,
@@ -89,10 +103,10 @@ fn main() {
                     let d: f64 = 2.0 * rng.gen::<f64>();
                     for _i in 0..12_000_000 {
                         let (x, y) = p.point_to_canvas_coordinate(&matrix2);
-                        image3.get_pixel_mut(x, y).blend(&gray);
+                        image.get_pixel_mut(x, y).blend(&gray);
                         p = de_jong_ifs(a, b, c, d, p);
                     }
-                    image3
+                    image
                         .save(format!("images/de-jong/day011-{}.png", i))
                         .unwrap();
                 });
