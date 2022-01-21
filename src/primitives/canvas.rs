155cbb8e71dc1fd3a7e4fedab845444c9df3b6cd
Edil Medeiros <j.edil@ene.unb.br>
2022-01-21 20:43:54 -0300

    Implement methods for Canvas

--- src/primitives/canvas.rs
+++ src/primitives/canvas.rs
@@ -1,27 +1,101 @@
-use image::{ImageBuffer, Pixel};
+use core::ops::{Deref, DerefMut};
+use std::path::Path;
+use image::{ImageBuffer, Pixel, ImageResult, EncodableLayout, Rgba};
+
+pub type RgbaCanvas = Canvas<Rgba<u8>, Vec<u8>>;
 
 #[derive(PartialEq, Debug)]
-pub struct Canvas<P: Pixel, Container> {
-    pub width: u32,
-    pub height: u32,
-    pub transform_matrix: TransformMatrix,
-    pub buffer: ImageBuffer<P, Container>,
+pub struct Canvas<P, Container>
+where
+    P: Pixel + 'static,
+    P::Subpixel: 'static,
+    Container: Deref<Target = [P::Subpixel]> + DerefMut,
+{
+    width: u32,
+    height: u32,
+//    transform_matrix: TransformMatrix,
+    buffer: ImageBuffer<P, Container>,
+}
+
+impl<P: Pixel, Container> Canvas<P, Container>
+where
+    P: Pixel + 'static,
+    P::Subpixel: 'static,
+    Container: Deref<Target = [P::Subpixel]> + DerefMut,
+{
+    pub fn put_pixel(&mut self, x: i32, y: i32, pixel: P) {
+        let sx: u32 = (self.width as i32 / 2 + x) as u32;
+        let sy: u32 = (self.height as i32 / 2 - y) as u32;
+
+        // Check if the given coordinates are visible.
+        // sx and sy are unsigned, only check upper bounds.        
+        if sx < self.width && sy < self.height {
+            self.buffer.put_pixel(sx, sy, pixel);
+        }
+    }
+}
+
+impl<P: Pixel + 'static> Canvas<P, Vec<P::Subpixel>>
+where
+    P::Subpixel: 'static
+{
+    pub fn new(width: u32, height: u32) -> Canvas<P, Vec<P::Subpixel>> {
+        Canvas {
+            width,
+            height,
+            buffer: ImageBuffer::new(width, height),
+        }
+    }
+
+    pub fn from_pixel(width: u32, height: u32, pixel: P) -> Canvas<P, Vec<P::Subpixel>> {
+        Canvas {
+            width,
+            height,
+            buffer: ImageBuffer::from_pixel(width, height, pixel),
+        }
+    }
+
+    pub fn from_fn<F>(width: u32, height: u32, f: F) -> Canvas<P, Vec<P::Subpixel>>
+    where
+        F: FnMut(u32, u32) -> P,
+    {
+        Canvas {
+            width,
+            height,
+            buffer: ImageBuffer::from_fn(width, height, f),
+        }
+    }
+}
+
+impl<P, Container> Canvas<P, Container>
+where
+    P: Pixel + 'static,
+    [P::Subpixel]: EncodableLayout,
+    Container: Deref<Target = [P::Subpixel]> + DerefMut,
+{
+    pub fn save<Q>(&self, path: Q) -> ImageResult<()>
+    where
+        Q: AsRef<Path>,
+    {
+        self.buffer.save(path)
+    }
 }
 
 pub trait Surface {
-    fn dimensions(&self) -> (u32, u32);
+    fn dimensions(&self) -> (i32, i32);
     fn matrix(&self) -> &TransformMatrix;
 }
 
-impl<P: Pixel, Container> Surface for Canvas<P, Container> {
-    fn dimensions(&self) -> (u32, u32) {
-        (self.width, self.height)
-    }
+// impl<P: Pixel> Surface for Canvas<P> {
+//     fn dimensions(&self) -> (i32, i32) {
+//         (self.width, self.height)
+//     }
 
-    fn matrix(&self) -> &TransformMatrix {
-        &self.transform_matrix
-    }
-}
+//     fn matrix(&self) -> &TransformMatrix {
+//         &self.transform_matrix
+//     }
+// }
+
 
 #[derive(PartialEq, Debug)]
 pub struct TransformMatrix {
