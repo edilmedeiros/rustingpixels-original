7a81017dc812cf288eae55240731611e49eb5b8b
Edil Medeiros <j.edil@ene.unb.br>
2022-01-21 18:40:24 -0300

    Add jpeg encoding

--- src/bin/day017/main.rs
+++ src/bin/day017/main.rs
@@ -1,12 +1,12 @@
-use image::{Pixel, Rgba};
+use image::{codecs::jpeg::JpegEncoder, Pixel, Rgba};
 use rand::prelude::*;
+use std::fs::File;
 //use rand_distr::{Normal, Distribution};
 use rustingpixels::primitives::canvas::*;
 
 fn main() {
     let width: u32 = 1080;
     let height: u32 = 1080;
-//    let black = Rgba([0, 0, 0, 255]);
 
     let palette1 = vec![
         Rgba([0xE6, 0x39, 0x46, 255]),
@@ -43,41 +43,46 @@ fn main() {
         Rgba([0xee, 0x9b, 0x00, 255]),
     ];
 
-//    let palettes = vec![palette1, palette2, palette3];
+    //    let palettes = vec![palette1, palette2, palette3];
     let mut rng = StdRng::seed_from_u64(5);
 
-    let bg = vec![&palette4[0], &palette1[4], &palette1[4], &palette2[6]];
-    let color = vec![&palette4[2], &palette1[0], &palette1[1], &palette2[0]];
+    let bgs = vec![palette4[0], palette2[6]];
+    let colors = vec![&palette4[2], &palette1[0], &palette1[1], &palette2[0]];
 
-    let mut image = image::RgbaImage::from_pixel(width, height, *bg[0]);
-    for k in 0..3 {
-        let transform = TransformMatrix {
-            xx: width as f64 / 20.0,
-            yx: 1.0,//((i % 2_u32) as f64).signum() * i_bound as f64,
-            xy: 1.0,//((j % 2_u32) as f64).signum() * j_bound as f64,
-            yy: height as f64 / 20.0,
-            x0: width as f64 / 2.0,
-            y0: height as f64 / 2.0,
-        };
+    for (k, bg) in bgs.iter().enumerate() {
+        let mut image = image::RgbaImage::from_pixel(width, height, *bg);
 
-        let density: u32 = rng.gen_range(500..1000);
-        for _j in 0..density {
-            let p = random_point_circular(50.0, &mut rng);
-            let (x, y) = p.point_to_canvas_coordinate(&transform);
-            let alpha: u8 = rng.gen_range(10..255);
+        for color in colors.iter() {
+            let transform = TransformMatrix {
+                xx: width as f64 / 20.0,
+                yx: 1.0, //((i % 2_u32) as f64).signum() * i_bound as f64,
+                xy: 1.0, //((j % 2_u32) as f64).signum() * j_bound as f64,
+                yy: height as f64 / 20.0,
+                x0: width as f64 / 2.0,
+                y0: height as f64 / 2.0,
+            };
 
-            for (xx,yy) in shape_coordinates(x,y) {
-                if point_is_visible(xx, yy, image.width(), image.height()) {
-                    image
-                        .get_pixel_mut(xx, yy)
-                        .blend(&color[k]
-                               .map_with_alpha(|x| x, |_| alpha));
+            let density: u32 = rng.gen_range(500..1000);
+            for _j in 0..density {
+                let p = random_point_circular(50.0, &mut rng);
+                let (x, y) = p.point_to_canvas_coordinate(&transform);
+                let alpha: u8 = rng.gen_range(10..255);
+
+                for (xx, yy) in shape_coordinates(x, y) {
+                    if point_is_visible(xx, yy, image.width(), image.height()) {
+                        image
+                            .get_pixel_mut(xx, yy)
+                            .blend(&color.map_with_alpha(|x| x, |_| alpha));
+                    }
                 }
             }
         }
-        
+        image.save(format!("images/day017-{}.png", k)).unwrap();
+
+        let mut buffer = File::create(format!("images/day017-{}.jpg", k)).unwrap();
+        let mut encoder = JpegEncoder::new_with_quality(&mut buffer, 100);
+        encoder.encode_image(&image).unwrap();
     }
-    image.save(format!("images/day017-{}.png", 0)).unwrap();
 }
 
 fn point_is_visible(x: u32, y: u32, width: u32, height: u32) -> bool {
@@ -107,8 +112,10 @@ fn point_is_visible(x: u32, y: u32, width: u32, height: u32) -> bool {
 // }
 
 fn random_point_circular<R>(radius: f64, rng: &mut R) -> Point<f64>
-where R: Rng {
-//    let normal = Normal::new(0.5, 0.3).unwrap();
+where
+    R: Rng,
+{
+    //    let normal = Normal::new(0.5, 0.3).unwrap();
     let r: f64 = (rng.gen::<f64>().sqrt() * radius).ln() * 2.5;
     let theta: f64 = rng.gen::<f64>() * 2.0 * std::f64::consts::PI;
     Point {
@@ -119,12 +126,26 @@ where R: Rng {
 
 fn shape_coordinates(x: u32, y: u32) -> Vec<(u32, u32)> {
     vec![
-        (x, y-2), (x,y-3),(x,y-4),
-        (x-1, y-1), (x, y-1), (x+1,y-1),
-        (x-1, y), (x+1, y), (x-2, y), (x+2, y),
-        (x-3,y), (x+3,y),(x-4,y), (x+4,y),
-        (x-1,y+1), (x,y+1), (x+1, y+1),
-        (x,y+2), (x, y+3),(x, y+4),
+        (x, y - 2),
+        (x, y - 3),
+        (x, y - 4),
+        (x - 1, y - 1),
+        (x, y - 1),
+        (x + 1, y - 1),
+        (x - 1, y),
+        (x + 1, y),
+        (x - 2, y),
+        (x + 2, y),
+        (x - 3, y),
+        (x + 3, y),
+        (x - 4, y),
+        (x + 4, y),
+        (x - 1, y + 1),
+        (x, y + 1),
+        (x + 1, y + 1),
+        (x, y + 2),
+        (x, y + 3),
+        (x, y + 4),
         //(x-2,y-2),(x+2,y-2),(x-2,y+2),(x+2,y+2),
     ]
 }
